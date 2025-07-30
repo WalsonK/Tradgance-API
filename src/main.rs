6000c14eaad56e87941496f1a60b07e4a19bfc1a
WalsonK <71435596+WalsonK@users.noreply.github.com>
2025-07-30 12:21:15 +0200

    feat: API + structure

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use axum::Router;
+use axum::routing::get;
+use dotenv::dotenv;
+
+mod routes;
+mod models;
+
+#[tokio::main]
+async fn main() {
+    dotenv().ok();
+
+    // routes
+    let app = Router::new()
+        .route("/", get(routes::hello::hello));
+
+    // run app
+    let addr = "127.0.0.1:3000";
+    let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
+    println!("Listening on http://{}", addr);
+
+    axum::serve(listener, app).await.unwrap();
 }
