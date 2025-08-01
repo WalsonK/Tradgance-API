8e04a1e377e1862d0b2d30e7f5d220b494bc4cd0
WalsonK <71435596+WalsonK@users.noreply.github.com>
2025-08-01 10:34:21 +0200

    init : binance

---
+++ src/binance/wallet.rs
@@ -0,0 +1,4 @@
+/*let client = wallet::rest_api::CapitalApiClient::new(configuration);
+    let params = client.all_coins_information(Default::default()).await.unwrap();
+    let data = params.data().await.unwrap();
+    println!("{:#?}", data);*/
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,29 @@
 use axum::Router;
 use axum::routing::get;
 use dotenv::dotenv;
+use binance_sdk::config::ConfigurationRestApi;
+use binance_sdk::wallet;
 
 mod routes;
 mod models;
+mod binance;
 
 #[tokio::main]
 async fn main() {
     dotenv().ok();
+
+    let configuration = ConfigurationRestApi::builder()
+        .api_key(dotenv::var("BINANCE_API_KEY").expect("API_KEY must be set"))
+        .api_secret(dotenv::var("BINANCE_PASS").expect("PASS must be set"))
+        .build()
+        .unwrap();
+
+    let client = wallet::WalletRestApi::production(configuration);
+    let params = wallet::rest_api::AccountInfoParams::default();
+    let response = client.account_info(params).await.unwrap();
+
+    let data = response.data().await.unwrap();
+    println!("{:#?}", data);
 
     // routes
     let app = Router::new()
